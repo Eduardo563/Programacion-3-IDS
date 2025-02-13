ba23e2bf4e97f2704c4851db5076202cd812eafb
lmmsh <lmmsh@LAPTOP-8E73N1CO>
2025-02-13 12:46:33 -0700

    unidad 2 ejercicio 7

--- VentanaJFrame/src/aplication/Ventana2.java
+++ VentanaJFrame/src/aplication/Ventana2.java
@@ -1,30 +1,70 @@
 package aplication;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Ventana2 extends JFrame{
 
+	Font etiquetas = new Font("Serif",Font.BOLD,18);
+
 	public Ventana2(String title) {
+		
 		this.setTitle(title);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(380, 500);
+		this.setSize(500, 500);
+		this.setMinimumSize(new Dimension(500,500));
+		this.setMaximumSize(new Dimension(600,600));
 	
 		this.setLocationRelativeTo(null);	
-		this.setResizable(false);
+		this.setResizable(true);
 		this.setLayout(null);
+		this.add(this.login());
+		
+		this.repaint();
+	}
+	
+	public JPanel login() {
+		
+		JPanel login = new JPanel();
+		login.setLayout(null);
+		login.setLocation(0, 0);
+		login.setSize(500, 500);
+		login.setOpaque(true);
+		login.setBackground(new Color (229,114,126));
+		login.setVisible(true);
+		
 		
 		JLabel etiqueta1 = new JLabel("Bienvenido");
-		etiqueta1.setSize(100, 100);
-		etiqueta1.setLocation(0, 0);
+		etiqueta1.setBounds(180, 150, 140, 35);
+		//etiqueta1.setSize(80, 30);
+		//etiqueta1.setLocation(160, 200);
 		etiqueta1.setBackground(Color.ORANGE);
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER);
+		etiqueta1.setVerticalAlignment(JLabel.CENTER);
+		etiqueta1.setFont(etiquetas);
 		etiqueta1.setOpaque(true);
 		
-		this.add(etiqueta1);
+		login.add(etiqueta1);
 		
+		JLabel etiqueta2 = new JLabel("Otro dato");
+		etiqueta2.setSize(100, 30);
+		etiqueta2.setLocation(200,190);
+		etiqueta2.setBackground(Color.WHITE);
+		etiqueta2.setHorizontalAlignment(JLabel.CENTER);
+		etiqueta2.setVerticalAlignment(JLabel.CENTER);
+		etiqueta2.setOpaque(true);
+		etiqueta2.setFont(etiquetas);
+		
+		login.add(etiqueta2);
+		
+		return login;
 	}
 
 }
