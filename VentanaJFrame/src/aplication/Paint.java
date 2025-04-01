089265043b99418ccdb838d071d865e686f431a8
lmmsh <lmmsh@LAPTOP-8E73N1CO>
2025-03-31 17:26:42 -0700

    Unidad 3 Ejercicio 20- Interfaz de Paint en Java

--- VentanaJFrame/src/aplication/Paint.java
+++ VentanaJFrame/src/aplication/Paint.java
@@ -2,29 +2,53 @@ package aplication;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.util.List;
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JSlider;
 import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Paint extends JFrame {
+public class Paint extends JFrame implements MouseListener, MouseMotionListener  {
 
 	
 	Border bordeSec = BorderFactory.createLineBorder(Color.GRAY, 3);
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	private ArrayList<Point> puntos = new ArrayList<Point>();
+	Color colorAct = Color.black;
+	int grosor;
+	List<List<Point>> listaDePuntos = new ArrayList<>();
+	List<Color> coloresUsados = new ArrayList<>();
+	List<Integer> grosores = new ArrayList<>();
+	lienzoConf lienzo;
 	/**
 	 * Launch the application.
 	 */
@@ -45,6 +69,10 @@ public class Paint extends JFrame {
 	 * Create the frame.
 	 */
 	public Paint() {
+		inicioPaint();
+	}
+	
+	public void inicioPaint() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1108, 745);
 		contentPane = new JPanel();
@@ -106,6 +134,24 @@ public class Paint extends JFrame {
 		tamaño.add(lblNewLabel);
 		
 		JSlider slider = new JSlider();
+		slider.setValue(3);
+        slider.setPaintLabels(true);
+ 
+        //indica el espaciado entre los valores
+        slider.setMajorTickSpacing(50);
+        slider.setMinorTickSpacing(5);
+ 
+        // setChangeListener
+        slider.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				
+				grosor = slider.getValue();	
+				
+			}
+        	
+        });
 		slider.setBounds(10, 27, 205, 26);
 		tamaño.add(slider);
 		
@@ -159,41 +205,113 @@ public class Paint extends JFrame {
 		seleCo.add(negro);
 		negro.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		negro.setBackground(Color.black);
+		negro.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.black;
+				
+			}
+			
+		});
 		
 		JButton rojo = new JButton("");
 		seleCo.add(rojo);
 		rojo.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		rojo.setBackground(Color.RED);
+		rojo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.red;
+				
+			}
+			
+		});
 		
 		JButton naranja = new JButton("");
 		seleCo.add(naranja);
 		naranja.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		naranja.setBackground(Color.orange);
+		naranja.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.orange;
+				
+			}
+			
+		});
 		
 		JButton amarillo = new JButton("");
 		seleCo.add(amarillo);
 		amarillo.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		amarillo.setBackground(Color.yellow);
+		amarillo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.yellow;
+				
+			}
+			
+		});
 		
 		JButton verde = new JButton("");
 		seleCo.add(verde);
 		verde.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		verde.setBackground(Color.green);
+		verde.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.green;
+				
+			}
+			
+		});
 		
 		JButton azul = new JButton("");
 		seleCo.add(azul);
 		azul.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		azul.setBackground(Color.blue);
+		azul.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.blue;
+				
+			}
+			
+		});
 		
 		JButton gris = new JButton("");
 		seleCo.add(gris);
 		gris.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		gris.setBackground(Color.GRAY);
+		gris.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.GRAY;
+				
+			}
+			
+		});
 		
 		JButton blanco = new JButton("");
 		seleCo.add(blanco);
 		blanco.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
 		blanco.setBackground(Color.white);
+		blanco.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				colorAct = Color.white;
+				
+			}
+			
+		});
 		
 		
 		JButton personalizado = new JButton("");
@@ -201,6 +319,20 @@ public class Paint extends JFrame {
 		personalizado.setBounds(31, 290, 65, 47);
 		colores.add(personalizado);
 		personalizado.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.gray));
+		JColorChooser colorPersonalizado = new JColorChooser(Color.black);
+		personalizado.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Color colorSelec = JColorChooser.showDialog(null,"Colores personalizados",colorAct);
+				//La condicion es para que si al momento de abrir el colorChooser, se presiona
+				// cancelar, se siga viendo en el trazo que se dibuje el ultimo color seleccionado
+				if (colorSelec != null) {
+					colorAct = colorSelec;
+				}
+			}
+			
+		});
 		
 		JButton btnNewButton_8 = new JButton("Limpiar todo");
 		btnNewButton_8.setForeground(new Color(255, 255, 255));
@@ -208,16 +340,143 @@ public class Paint extends JFrame {
 		colores.add(btnNewButton_8);
 		btnNewButton_8.setBackground(new Color(239, 68, 54));
 		btnNewButton_8.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 13));
+		btnNewButton_8.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//Se debe limpiar el panel y tambien liberar todas las listas para no tener 
+				// informacion de trazos anteriores
+				lienzo.removeAll();
+				puntos.clear();
+				listaDePuntos.clear();
+				grosores.clear();
+				coloresUsados.clear();
+				repaint();
+			}
+			
+		});
 		
 		JPanel fondoLienzo = new JPanel();
 		fondoLienzo.setBackground(new Color(192, 192, 192));
 		contentPane.add(fondoLienzo, BorderLayout.CENTER);
 		fondoLienzo.setLayout(null);
 		
-		JPanel lienzo = new JPanel();
+		lienzo = new lienzoConf();
 		lienzo.setBounds(10, 11, 990, 729);
 		lienzo.setBorder(BorderFactory.createLineBorder(Color.black, 2));
-		fondoLienzo.add(lienzo);
 		lienzo.setLayout(null);
+		lienzo.addMouseListener(this);
+		lienzo.addMouseMotionListener(this);
+		fondoLienzo.add(lienzo);
 	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+		//crear una copia de los puntos
+		ArrayList ArrList2  = (ArrayList)puntos.clone();
+		
+		//se guarda en el historial de dibujos
+		listaDePuntos.add(ArrList2);
+		
+		//Guardar el color usado
+		coloresUsados.add(colorAct);
+		
+		//Guardar el grosor del pincel
+		grosores.add(grosor);
+		
+		//limpiamos el trazo actual
+		puntos.clear();
+		
+		lienzo.repaint();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) { 
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) { 
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		lienzo.repaint();
+		
+		puntos.add(e.getPoint());
+		
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	//Se usa una clase interna para modificar el componente graphics 
+	class lienzoConf extends JPanel{
+		public lienzoConf() {
+			this.setBackground(Color.white);
+		}
+		
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			Graphics2D g2 = (Graphics2D) g;
+			
+			int posicionColorGrosor=0;
+		       for (Iterator iterator = listaDePuntos.iterator(); iterator.hasNext();posicionColorGrosor++) {
+				List<Point> trazo = (List<Point>) iterator.next();
+				
+				Color color = coloresUsados.get(posicionColorGrosor);
+				g2.setColor(color);
+				g2.setStroke(new BasicStroke(grosores.get(posicionColorGrosor)));
+				
+					if(trazo.size()>1) {
+			    	   
+			    	   for (int i = 1; i < trazo.size(); i++) {
+			    		   
+			    		   Point p1 = trazo.get(i-1);
+			    		   Point p2 = trazo.get(i);
+			    		   
+			    		   
+			    		   g2.drawLine(p1.x,p1.y,p2.x,p2.y);
+			    	   }
+			    	   
+			       }
+				
+		       }
+			
+			g2.setColor(colorAct);
+			g2.setStroke(new BasicStroke(grosor));
+			
+			if (puntos.size()>1) {
+		    	   
+		    	   for (int i = 1; i < puntos.size(); i++) {
+		    		   
+		    		   Point p1 = puntos.get(i-1);
+		    		   Point p2 = puntos.get(i);
+		    		   
+		    		   g2.drawLine(p1.x,p1.y,p2.x,p2.y);
+		    	   }
+		    	   
+		       }
+			   
+		}
+	}
+
 }
